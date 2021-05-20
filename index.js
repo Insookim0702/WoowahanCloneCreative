2e4b52d3b1b3adfe49ff701d0aca106eb296222a
insookim0702 <insookim0702@gmail.com>
2021-05-20 22:17:50 +0900

    내용 수정

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 (() => {
-  const BUTTON_3 = document.querySelector(".button_3");
-  const F_CONTENT = document.querySelector(".fcontent");
-  function init() {
-    F_CONTENT.style.display = "none";
-    BUTTON_3.addEventListener("click", () => {
-      if (F_CONTENT.style.display === "none") {
-        F_CONTENT.style.display = "flex";
-      } else {
-        F_CONTENT.style.display = "none";
-      }
-    });
-  }
+    const BODY = document.body;
+    const BUTTON_1 = document.querySelector('.button_1'); // 위
+    const BUTTON_2 = document.querySelector('.button_2'); // 아래
+    const BUTTON_3 = document.querySelector('.button_3');
+    const FOOTER = document.querySelector('.footer_child');
+    const F_CONTENT = document.querySelector('.fcontent');
+    function init() {
+        F_CONTENT.style.display = 'none';
+        BUTTON_3.addEventListener('click', () => {
+            if (F_CONTENT.style.display === 'none') {
+                F_CONTENT.style.display = 'flex';
+            } else {
+                F_CONTENT.style.display = 'none';
+            }
+        });
 
-  init();
+        BUTTON_1.addEventListener('click', () => {
+            window.scroll({
+                behavior: 'smooth',
+                left: 0,
+                top: BODY.offsetTop,
+            });
+        });
+
+        BUTTON_2.addEventListener('click', () => {
+            window.scroll({
+                behavior: 'smooth',
+                left: 0,
+                top: FOOTER.offsetTop,
+            });
+        });
+    }
+
+    init();
 })();
