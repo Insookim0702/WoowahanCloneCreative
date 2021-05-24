d51ba73f6cdd9e2ba12a63162fae4f6becb227da
insookim0702 <insookim0702@gmail.com>
2021-05-24 22:45:53 +0900

    프로젝트 상세 '더보기'&'접기' 버튼 기능 추가

--- src/js/moreBtn.js
+++ src/js/moreBtn.js
@@ -2,7 +2,21 @@
     const FUNC_DESC_MORE_BTN = document.querySelector('.func_desc_more_btn');
     const FUNC_DESC_MORE = document.querySelector('.func_desc_more');
     const FUNC_DESC_CUT_BTN = document.querySelector('.func_desc_cut_btn');
+    const MORE_BTN_LIST = document.querySelectorAll('.more_btn');
+    const REDUCE_BTN_LIST = document.querySelectorAll('.reduce_btn');
+    const PROJECT_LIST = document.querySelectorAll('.project');
+    const PROJECT_CONTENT_LIST = document.querySelectorAll('.project_content');
+
     function init() {
+        console.log(PROJECT_CONTENT_LIST);
+
+        let projectList = [];
+        PROJECT_CONTENT_LIST.forEach((content) => {
+            projectList.push(content.id);
+        });
+
+        console.log(projectList);
+
         FUNC_DESC_MORE_BTN.addEventListener('click', () => {
             FUNC_DESC_MORE.style.display = 'block';
             FUNC_DESC_MORE_BTN.style.display = 'none';
@@ -11,6 +25,24 @@
         FUNC_DESC_CUT_BTN.addEventListener('click', () => {
             FUNC_DESC_MORE_BTN.style.display = 'block';
             FUNC_DESC_MORE.style.display = 'none';
+        });
+
+        MORE_BTN_LIST.forEach((btn) => {
+            btn.addEventListener('click', (event) => {
+                PROJECT_LIST[event.target.id].style.height = `${PROJECT_CONTENT_LIST[event.target.id].offsetHeight}px`;
+                //접기 버튼 보이기
+                REDUCE_BTN_LIST[event.target.id].style.display = 'flex';
+                MORE_BTN_LIST[event.target.id].style.display = 'none';
+            });
+        });
+
+        REDUCE_BTN_LIST.forEach((btn) => {
+            btn.addEventListener('click', (event) => {
+                PROJECT_LIST[event.target.id].style.height = `704px`;
+                // 더보기 버튼 보이기
+                REDUCE_BTN_LIST[event.target.id].style.display = 'none';
+                MORE_BTN_LIST[event.target.id].style.display = 'flex';
+            });
         });
     }
 
